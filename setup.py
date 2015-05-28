ef882a30404e39a571a3e0efc2dff335c923160d
James Turk <james.p.turk@gmail.com>
2015-05-28 14:17:15 -0400

    tox

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='fkreplace',
+    version="0.1.0",
+    package_dir={'fkreplace': 'fkreplace'},
+    packages=['fkreplace'],
+    description='',
+    long_description='',
+    author='James Turk',
+    author_email='james.p.turk@gmail.com',
+    license='BSD',
+    url='https://github.com/jamesturk/fkreplace/',
+    platforms=["any"],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
